eb70eef2b0891a05772f9157d7f6cf94c4874c13
OlegYaschenko <olegyaschenko@gmail.com>
2021-12-15 16:53:30 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,10 @@ const inputElement = document.querySelector('#validation-input');
 inputElement.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) { 
-  (Number(inputElement.dataset.length) === event.currentTarget.value.length)
-    ? inputElement.classList.add('valid')
-    : inputElement.classList.add('invalid')
+  if (Number(inputElement.dataset.length) === event.currentTarget.value.length) {
+    inputElement.classList.add('valid');
+  } else { 
+    inputElement.classList.add('invalid');
+  }
 }
 
